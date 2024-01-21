a650447250bda814b040a1eb90c152be2cd8a7ab
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2024-01-20 22:45:52 -0300

    fix: adding observable

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -2,6 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,7 @@ export class EmailService {
 
   constructor(private http: HttpClient) {}
 
-  sendEmail(data: any) {
+  sendEmail(data: any): Observable<any> {
     return this.http.post(this.formSpreeEndpoint, data);
   }
 }
